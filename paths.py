acd14a91e5ad1c69eeb9e7f2a69e25311640a326
Shayenne <shayenne.moura@usp.br>
2018-02-21 17:17:17 -0300

    Create notebook with VAD based on energy

--- paths.py
+++ paths.py
@@ -1,10 +1,10 @@
 import os
 
 os.environ["MUSIC_PATH"] = "/home/compmus/shayenne/2017/IA/MedleyDB/Audio/MusicDelta_Rock_MIX.wav"
-os.environ["MEDLEYDB_PATH"] = "/home/compmus/shayenne/2017/IA/MedleyDB"
+os.environ["MEDLEYDB_PATH"] = "/home/compmus/shayenne/2017/IA/MedleyDB/"
 
 os.environ["AUDIO_PATH"] = "/home/compmus/shayenne/2017/IA/MedleyDB/Audio/"
 os.environ["ANNOT_PATH"] = "/home/compmus/shayenne/2017/IA/MedleyDB/Annotations/"
 
-os.environ["FEATURE_PATH"] = "/home/compmus/shayenne/2018/VoiceDetection/MedleyDB/features"
-os.environ["LABEL_PATH"] = "/home/compmus/shayenne/2018/VoiceDetection/MedleyDB/labels"
+os.environ["FEATURE_PATH"] = "./MedleyDB/features/"
+os.environ["LABEL_PATH"] = "./MedleyDB/labels/"
