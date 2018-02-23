d55758a2594803eb877bdf59ed6531fa04f401fa
Shayenne <shayenne.moura@usp.br>
2018-02-23 14:50:23 -0300

    Start use Essentia features

--- EllisApproach/paths.py
+++ EllisApproach/paths.py
@@ -2,10 +2,9 @@ import os
 
 os.environ["MUSIC_PATH"] = "/home/compmus/MIR-1K/Wavfile/abjones_1_01.wav"
 os.environ["VOCAL_PATH"] = "/home/compmus/MIR-1K/vocal-nonvocalLabel/abjones_1_01.vocal"
-os.environ["MEDLEYDB_PATH"] = "/home/compmus/shayenne/2017/IA/MedleyDB/"
 
 os.environ["AUDIO_PATH"] = "/home/compmus/MIR-1K/Wavfile/"
 os.environ["ANNOT_PATH"] = "/home/compmus/MIR-1K/vocal-nonvocalLabel/"
 
-os.environ["FEATURE_PATH"] = "./MedleyDB/features/"
-os.environ["LABEL_PATH"] = "./MedleyDB/labels/"
+os.environ["FEATURE_PATH"] = "/home/compmus/MIR-1K/spectrogram/"
+os.environ["LABEL_PATH"]   = "/home/compmus/MIR-1K/PitchLabel/"
---
+++ EllisApproach/vad_cls.py
@@ -0,0 +1,118 @@
+import paths                        # Environment paths
+import os                           # Manipulate paths
+import numpy as np                  # Numerical processing
+import librosa                      # Audio processing
+import matplotlib.pyplot as plt     # Plot graphs
+import csv                          # Manipulate csv files
+import librosa.display
+import pandas as pd
+
+import paths
+
+
+"""
+Calculate features
+"""
+def calculate_features():
+
+    # All files selected from MedleyDB
+    audio_path = os.path.join(os.environ['AUDIO_PATH'])
+
+    test = 0
+    for filename in os.listdir(audio_path):
+        if filename.endswith(".wav"):
+            music = filename
+            music = music[:-4]
+        else:
+            continue
+
+        print ("--- Preprocessing...")
+        print (filename)
+
+        # Load audio file
+        y, orig_sr = librosa.load(audio_path+filename, mono=True) 
+
+        # Resample
+        #target_sr = 8000
+        #y_res = librosa.resample(y, orig_sr, target_sr)
+
+        print ("> Audio signal loaded...")
+        hl = int(0.0020*orig_sr) # 20 milisseconds
+        wl = int(0.0040*orig_sr) # 40 milisseconds
+        
+        # STFT analysis (Parameters based on article [1])
+        S = librosa.stft(y, n_fft=1024, hop_length=hl,
+                         win_length=wl, window='hann')
+        D = librosa.amplitude_to_db(S, ref=np.max)
+
+        # Remove frequencies above 2kHz
+        #D_cut = D[:256, :]
+
+        
+
+        print("Writing features file...")
+
+    
+        # Write features file
+        feature_path = os.path.join(os.environ['FEATURE_PATH'],
+                                    music+'_features.csv')
+    
+        with open(feature_path, 'w') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            col = D.shape[1]
+            for i in range(col):
+                spamwriter.writerow(D[:,i])
+        print("DONE")
+
+        test +=1
+
+        if test > 0:
+            break
+
+"""
+Load all files
+"""
+
+# Input Data
+def load_data(feature_path=None, label_path=None):
+    """ Receives features and labels path and return it on a list [X,y],
+        where 'X' has the feature matrix and 'y' has the vector label.
+    """
+
+    if feature_path is None:
+        feature_path = os.path.join(os.environ['FEATURE_PATH'])
+    if label_path is None:
+        label_path = os.path.join(os.environ['LABEL_PATH'])
+
+    list_feat = []
+    list_lbl  = []
+    i = 0
+    for filename in os.listdir(feature_path):
+        if filename.endswith(".csv"):
+            music = filename
+            music = music[:-12] # Remove 'features.csv'
+        else:
+            continue
+        # Read STFT from csv files
+        print("   Load", feature_path + music + "features.csv")
+        d1 = pd.read_csv(feature_path + music + "features.csv",
+                         index_col=None, header=None)
+        list_feat.append(d1)
+        print("   Load", label_path + music + "labels.csv")
+        d2 = pd.read_csv(label_path + music + "labels.csv",
+                         index_col=None, header=None)
+        list_lbl.append(d2)
+        # Fitting each music
+        print(d1.shape, d2.shape)
+        i += 1
+
+        if i == 1:
+            break
+
+    # Grouping data
+    X = pd.concat(list_feat)
+    y = pd.concat(list_lbl)
+
+    # Return data
+    return [X, np.ravel(y)]
